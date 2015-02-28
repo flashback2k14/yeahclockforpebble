2d58ab0241f0058982fb2c44abd69791e7a87f21
flashback2k14 <feedbackcalcest@gmail.com>
2015-03-01 00:45:21 +0100

    test input

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,29 @@
 console.log('This would be the main JS file.');
+function saveOptions() {
+	var options = {}
+	//Add all textual values
+	$('textarea, select, [type="hidden"], [type="password"], [type="text"]').each(function(){options[$(this).attr('id')] = $(this).val();})
+	return options;
+}
+
+$().ready(function() {
+	$("#b-cancel").click(function() {
+	  console.log("Cancel");
+	  document.location = "pebblejs://close";
+	});
+	
+	$("#b-submit").click(function() {
+	  console.log("Submit");
+	  var location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(saveOptions()));
+	  console.log("Warping to: " + location);
+	  console.log(location);
+	  document.location = location;
+	});
+	
+	//Set form values to whatever is passed in.
+	var obj = jQuery.parseJSON(decodeURIComponent(window.location.search.substring(1)));
+	for(key in obj) {
+	  $("#"+[key]).val(obj[key]);
+	  $("#"+[key]).val(obj[key]).slider("refresh");       
+	}
+});
