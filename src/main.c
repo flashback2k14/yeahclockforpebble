2df1ffd6bcd787dc124cc76c6dc7bf85949f872c
Sebastian Kloppe <feedbackcalcest@gmail.com>
2015-03-01 17:14:32 +0100

    bugfix update time handler

--- src/main.c
+++ src/main.c
@@ -9,14 +9,16 @@ static TextLayer *s_date_layer;
 static GFont s_time_font;
 static GFont s_date_font;
 /**
- * update hour time function
+ * update time function
  */
-static void update_time_hour() {
+static void update_time() {
   // Get a tm structure
   time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
   // Create a long-lived buffer
   static char buffer_h[] = "00";
+  static char buffer_min[] = "00";
+  static char buffer_date[] = "Mon, 01.01.15";
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
 		// Use 24 hour format
@@ -25,60 +27,18 @@ static void update_time_hour() {
     	// Use 12 hour format
 		strftime(buffer_h, sizeof("00"), "%I", tick_time);  
     }
+	strftime(buffer_min, sizeof("00"), "%M", tick_time);
+	strftime(buffer_date, sizeof("Mon, 01.01.15"), "%a, %d.%m.%y", tick_time);
   	// Display this time on the TextLayer
   	text_layer_set_text(s_hours_layer, buffer_h);	
-}
-/**
- * update minutes time function
- */
-static void update_time_minutes() {
-  // Get a tm structure
-  time_t temp = time(NULL); 
-  struct tm *tick_time = localtime(&temp);
-  // Create a long-lived buffer
-  static char buffer_min[] = "00";
-  // Write the current hours and minutes into the buffer
-  if(clock_is_24h_style() == true) {
-		// Use 24 hour format
-		strftime(buffer_min, sizeof("00"), "%M", tick_time);
-    } else {
-    	// Use 12 hour format
-		strftime(buffer_min, sizeof("00"), "%M", tick_time);
-    }
-  	// Display this time on the TextLayer
-  	text_layer_set_text(s_minutes_layer, buffer_min);
-}
-/**
- * update date time function
- */
-static void update_time_date() {
-	  // Get a tm structure
-	  time_t temp = time(NULL); 
-	  struct tm *tick_time = localtime(&temp);
-	  // Create a long-lived buffer
-	  static char buffer_min[] = "Mon, 01.01.15";
-	  // Write the current hours and minutes into the buffer
-	  strftime(buffer_min, sizeof("Mon, 01.01.15"), "%a, %d.%m.%y", tick_time);
-	  // Display this time on the TextLayer
-  	  text_layer_set_text(s_date_layer, buffer_min);
+	text_layer_set_text(s_minutes_layer, buffer_min);
+	text_layer_set_text(s_date_layer, buffer_date);
 }
 /**
  * handle  time function hour
  */
-static void tick_handler_hour(struct tm *tick_time, TimeUnits units_changed) {
-	update_time_hour();
-}
-/**
- * handle  time function minutes
- */
-static void tick_handler_min(struct tm *tick_time, TimeUnits units_changed) {
-	update_time_minutes();
-}
-/**
- * handle  time function date
- */
-static void tick_handler_date(struct tm *tick_time, TimeUnits units_changed) {
-	update_time_date();
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+	update_time();
 }
 /**
  * init Watchface layout
@@ -87,7 +47,7 @@ static void main_window_load(Window *window) {
 	//create GFont
 	s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_ROBOTO_SLAB_64));
 	s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_ROBOTO_SLAB_20));
-	//create time textlayer
+	//create textlayers
 	s_hours_layer = text_layer_create(GRect(0, -5,144,70));
 	s_date_layer = text_layer_create(GRect(0, 72,144,40));
 	s_minutes_layer = text_layer_create(GRect(0, 95,144,70));
@@ -126,12 +86,10 @@ static void main_window_load(Window *window) {
 	text_layer_set_text_alignment(s_minutes_layer, GTextAlignmentCenter);
 	//add as child to window
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_hours_layer));
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_minutes_layer));
-	//register with TickTimerService
-	tick_timer_service_subscribe(HOUR_UNIT, tick_handler_hour);
-	tick_timer_service_subscribe(MINUTE_UNIT, tick_handler_min);
-	tick_timer_service_subscribe(DAY_UNIT, tick_handler_date);
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
+	//Make sure the time is displayed from the start
+	update_time();
 }
 /**
  * deinit Watchface layout
@@ -139,8 +97,8 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
 	//destroy textlayer
 	text_layer_destroy(s_hours_layer);
-	text_layer_destroy(s_date_layer);
 	text_layer_destroy(s_minutes_layer);
+	text_layer_destroy(s_date_layer);
 	//unload GFont
 	fonts_unload_custom_font(s_time_font);
 	fonts_unload_custom_font(s_date_font);
@@ -166,10 +124,8 @@ static void init() {
 	});
 	//show window on pebble with animation
 	window_stack_push(s_main_window, true);
-	//Make sure the time is displayed from the start
-	update_time_hour();
-	update_time_minutes();
-	update_time_date();
+	//register with TickTimerService
+	tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 /**
  * deinit watchface
