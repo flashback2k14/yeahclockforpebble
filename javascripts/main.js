02f23c2c42effff0e7e4c0dd3e81e3bd6453f784
flashback2k14 <feedbackcalcest@gmail.com>
2015-03-02 12:30:39 +0100

    rework js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,28 +1,26 @@
 function saveOptions() {
-	var options = {}
-	//Add all textual values
-	$('select, [type="password"], [type="text"]').each(function(){options[$(this).attr('id')] = $(this).val();})
+	var invertSelect = document.getElementById("selectInvert");
+	var options = {
+		"invertColors": invertSelect.options[invertSelect.selectedIndex].value
+	}
 	return options;
 }
 
-$().ready(function() {
-	$("#btnCancel").click(function() {
-	  console.log("Cancel");
-	  document.location = "pebblejs://close";
-	});
-	
-	$("#btnSubmit").click(function() {
-	  console.log("Submit");
-	  var location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(saveOptions()));
-	  console.log("Warping to: " + location);
-	  console.log(location);
-	  document.location = location;
-	});
-	
-	//Set form values to whatever is passed in.
-	/**var obj = jQuery.parseJSON(decodeURIComponent(window.location.search.substring(1)));
-	for(key in obj) {
-	  $("#"+[key]).val(obj[key]);
-	  $("#"+[key]).val(obj[key]).slider("refresh");       
-	}*/
-});
+function submitCustomization() {
+	console.log("Submit");
+	var options = saveOptions();
+	var location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(options));
+	console.log("Warping to: " + location);
+	document.location = location;
+}
+
+function cancelCustomization(){
+	console.log("Cancel");
+	document.location = "pebblejs://close";
+}
+
+var submitButton = document.getElementById("btnSubmit");
+submitButton.addEventListener("click", submitCustomization,false);
+
+var cancelButton = document.getElementById("btnCancel");
+cancelButton.addEventListener("click", cancelCustomization, false);
