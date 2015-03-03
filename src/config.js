5af9e37bbe8a0db9910036050e61f648cb5bade1
Sebastian Kloppe <feedbackcalcest@gmail.com>
2015-03-03 12:58:15 +0100

    change config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 /**
  * https://ninedof.wordpress.com/2014/05/24/pebble-sdk-2-0-tutorial-9-app-configuration/
  * https://github.com/pebble-hacks/js-configure-demo/blob/master/src%2Fjs%2Fpebble-js-app.js
+ * https://github.com/pebble-examples/watchface-tutorial/blob/master/src%2Fwatchface-tutorial.c
  * http://www.sitepoint.com/advanced-pebble-watch-configuration/
  * http://developer.getpebble.com/guides/js-apps/pebblekit-js/app-configuration/#tip-use-json-to-transfer-configuration-information
  * http://stackoverflow.com/questions/24230407/creating-a-configuration-page-and-passing-variables-to-a-simply-js-app
@@ -20,14 +21,12 @@ Pebble.addEventListener("showConfiguration", function() {
 
 Pebble.addEventListener("webviewclosed", function(e) {
   console.log("configuration closed");
-  // webview closed
-  //Using primitive JSON validity and non-empty check
+  //JSON validity and non-empty check
   if (e.response.charAt(0) == "{" && e.response.slice(-1) == "}" && e.response.length > 5) {
     options = JSON.parse(decodeURIComponent(e.response));
     console.log("Options = " + JSON.stringify(options));
-	 
 	//Send to Pebble, persist there
-	Pebble.sendAppMessage({"KEY_INVERT": options.name},
+	Pebble.sendAppMessage({"KEY_INVERT": options.invertColors},
       function(e) {
         console.log("Sending settings data...");
       },
