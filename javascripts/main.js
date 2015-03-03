8b3bb6539bdb71b5739a3f4c0b8f793f94c19e99
flashback2k14 <feedbackcalcest@gmail.com>
2015-03-03 13:01:46 +0100

    test js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,54 +1,22 @@
-function saveOptions() {
-	var invertSelect = document.getElementById("selectInvert");
-	var options = {
-		"invertColors": invertSelect.options[invertSelect.selectedIndex].value
-	}
-	return options;
-}
+$(document).ready( function () {
 
-/**function submitCustomization() {
-	console.log("Submit");
-	var options = saveOptions();
-	var location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(options));
-	console.log("Warping to: " + location);
-	document.location = location;
-}
+  function saveOptions() {
+    var invertSelect = document.getElementById("selectInvert");
+    var options = {
+      "invertColors": invertSelect.options[invertSelect.selectedIndex].value
+    }
+    return options;
+  }
 
-function cancelCustomization(){
-	console.log("Cancel");
-	document.location = "pebblejs://close";
-}*/
+  $('#btnCancel').on('click', function () {
+    console.log("Cancel");
+    document.location = "pebblejs://close";
+  });
 
-/**var submitButton = document.getElementById("btnSubmit");
-submitButton.addEventListener("click", 
-	function() {
-		console.log("Submit");
-		var options = saveOptions();
-		var location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(options));
-		console.log("Warping to: " + location);
-		document.location = location;
-	}
-,false);
-
-var cancelButton = document.getElementById("btnCancel");
-cancelButton.addEventListener("click", 
-	function(){
-		console.log("Cancel");
-		document.location = "pebblejs://close";
-	}
-,false);*/
-
-$().ready(function() {
-	$("#btnCancel").click(function() { 	 
-		console.log("Cancel"); 	 
-		document.location = "pebblejs://close"; 	
-	}); 
-	
-	$("#btnSubmit").click(function() { 	 
-		console.log("Submit"); 	 
-		var location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(saveOptions())); 	
-		console.log("Warping to: " + location); 	 
-		console.log(location); 	 
-		document.location = location; 	
-	});
+  $('#btnSubmit').on('click', function () {
+    console.log("Submit");
+    var location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(saveOptions()));
+    console.log(location);
+    document.location = location;
+  });
 });
