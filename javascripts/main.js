037c51db6523c5817ab6155b3028cc3819809c08
flashback2k14 <feedbackcalcest@gmail.com>
2015-03-03 14:09:24 +0100

    test js again

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,4 @@
-$(document).ready( function () {
-
+/**$(document).ready( function () {
   function saveOptions() {
     var invertSelect = document.getElementById("selectInvert");
     var options = {
@@ -19,4 +18,33 @@ $(document).ready( function () {
     console.log(location);
     document.location = location;
   });
+});*/
+
+function saveOptions() {
+  var options = {}
+  $('select, [type="text"]').each(function(){options[$(this).attr('id')] = $(this).val();})
+  return options;
+}
+
+$().ready(function() {
+  $("#btnCancel").click(function() {
+    console.log("Cancel");
+    document.location = "pebblejs://close";
+  });
+
+  $("#btnSubmit").click(function() {
+    console.log("Submit");
+    var location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(saveOptions()));
+    console.log("Warping to: " + location);
+    console.log(location);
+    document.location = location;
+  });
+
+  //Set form values to whatever is passed in.
+  if (decodeURIComponent(window.location.search.substring(1)) === undefined) {
+    var obj = jQuery.parseJSON(decodeURIComponent(window.location.search.substring(1)));
+    for(key in obj) {
+      $("#"+[key]).val(obj[key]);
+    }
+  }
 });
